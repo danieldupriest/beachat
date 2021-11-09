080bd6e1eefced80f7c954319fbef744f7f8c875
kououken <kououken@gmail.com>
2021-11-09 15:48:44 -0800

    Client styled, enabled autoscroll

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="page">
-    <h2>Beachat</h2>
-    <code>
+  <div id="page">
+    <h2 id="title">Beachat</h2>
+    <code id="terminal">
       <p v-for="(line, index) in terminal" :key="index">
         {{line}}
       </p>
       </code>
-    <input type="text" v-on:keyup.enter="send" v-model="input" />
+    <input id="input" type="text" v-on:keyup.enter="send" v-model="input" placeholder="Type message here"/>
   </div>
 </template>
 
@@ -29,6 +29,8 @@ export default {
     },
     addLine(line) {
       this.terminal.push(line)
+      const terminal = document.getElementById("terminal")
+      terminal.scrollTop = terminal.scrollHeight
     },
     connect(name) {
       this.addLine("Connecting to server")
@@ -42,7 +44,7 @@ export default {
       }
     }
   },
-  created() {
+  mounted() {
     const randomUserName = "user" + Math.floor(Math.random() * 9999)
     const name = prompt("Enter a user name", randomUserName)
     this.connect(name)
@@ -51,4 +53,50 @@ export default {
 </script>
 
 <style>
+html, body {
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+#app {
+  background: #333;
+  min-height: 100vh;
+}
+#page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  max-width: 80ch;
+  margin: 0 auto;
+}
+#title {
+    background: -webkit-linear-gradient(#aaf, #3a3);
+    font-size: 300%;
+    background-clip: text;
+    height: 3rem;
+    -webkit-text-fill-color: transparent;
+    padding: .25em 0;
+    margin: 0;
+}
+#terminal {
+  background: #444;
+  border-radius: 15px;
+  color: #eee;
+  display: block;
+  height: calc(100vh - 10rem);
+  overflow-y: scroll;
+  padding: 1em;
+}
+#terminal p {
+  margin: .25em 0 0 0;
+}
+#input {
+  background: #444;
+  border-radius: 15px;
+  color: #eee;
+  font-family: monospace;
+  height: 1rem;
+  margin-top: 1em;
+  padding: .5em 1em .5em 1em;
+}
 </style>
